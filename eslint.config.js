5509d93b0565dde63e361cbcb95c182b443f6f46
Octaleads <octaleadsprivatelimited@gmail.com>
2025-10-25 05:54:51 +0000

    Fix deprecation warnings: Update dependencies and ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'prefer-const': 'error'
+    }
+  }
+];
